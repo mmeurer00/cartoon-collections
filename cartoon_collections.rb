09f28f662930052b788137e8689fcac4f5d61a4a
Maxine <maxinemeurer@yahoo.com>
2021-02-04 23:41:59 -0500

    commit tree 809fbb1ed8cad880388497ba98549af56a091027

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  counter = 1
+  dwarves.each do | name |
+    puts "#{counter}. #{name}"
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect do |call|
+    "#{call.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_short)
+  if calls_short.find{|i| 4 < i.size }
+    true
+  else 
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |cheese| cheese_types.include?(cheese) }
 end
+
